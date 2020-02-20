95201640de6ce906bc78d759424da5941a461ada
amira <amira.a.a.hashim@gmail.com>
2020-02-20 17:21:11 +0200

    changing settings

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from . import forms
 from blog.forms import UserForm
 from django.views.generic import CreateView
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import authenticate
 from django.urls import reverse_lazy, reverse
 from django.contrib.auth import login, logout
 from django.http import HttpResponse, HttpResponseRedirect
@@ -34,7 +34,7 @@ class SignUp(CreateView):
                 raw_password = form.cleaned_data.get('password1')
                 user = authenticate(username=user.username, password=raw_password)
                 login(request, user)
-                return HttpResponseRedirect("login")
+                return redirect("login")
         else:
             form = UserForm()
             return render(request, 'html/signup.html', {'form': form})
--- pysite/settings.py
+++ pysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'blog',
 ]
 
@@ -116,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR,'blog/static/')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
