a454cd9b9c6687ac71859e12a3c02c2b156a6632
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-02-22 14:29:06 +0200

    make static files scoop is the whole project

--- pysite/settings.py
+++ pysite/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR,'blog/static/')
+STATIC_URL = '/static/'
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR,'static'),
+)
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
